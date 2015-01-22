5c5e57c7c655cc112feece083b22f2ac6b28c58b
Matt Rich <metropolis_4@hotmail.com>
2015-01-22 14:44:28 -0700

    consolidate .js .css

--- main.js
+++ main.js
@@ -1,10 +1,52 @@
 $(document).on('ready', function() {
+
+	// index.html
+
   $('#submit').click(function() {
   	var add = $("input[name=statement").val();
-  	$('.userInput').append("<p>" + add + "</p>");
+  	$('.userInput').append("<p class='post'>" + add + "</p>");
   });
   $('#list').click(function() {
   	$('.userInput').append("<ul><li></li><li></li><li></li></ul>");
   });
+
+// index2.html
+
+$('.selected').hover(
+  	function() {
+  	$(this).addClass("active").append("<span>!!!</span>");
+  	},
+  	function() {
+  	$(this).removeClass("active").children("span").remove();
+  });
+
+  $('a').on("click", function(e) {
+  	var userInput = confirm("Are you sure you want to leave this page?");
+  	if(userInput === false) {
+  		e.preventDefault();
+  		$('button').remove();
+  	}
+  	
+  });
+
+  // index3.html
+
+  $('.bigButton').click(function() {
+		$('.bigButton').fadeOut(400);
+		
+		$('.main-content').append(
+			"<div class='popup'>" +
+				"<h2>hello...</h2>" +
+				"<button class='close buttonMain'><span>goodbye...</span></button>" +
+			"</div>");
+
+		$('.close').hide().delay(400).fadeIn(4000).animate({top: '-150px'}, 2000);
+		$('h2').fadeOut(4000);
+
+		$('.close').click(function() {
+		$('.popup').detach();
+		$('.bigButton').fadeIn(600);
+	});
+	});
 
 });
