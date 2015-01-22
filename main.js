6f31611e44c233a87211ad61441ace3e7b586920
Matt Rich <metropolis_4@hotmail.com>
2015-01-22 09:41:18 -0700

    Fixed Search Button

--- main.js
+++ main.js
@@ -3,4 +3,8 @@ $(document).on('ready', function() {
   	var add = $("input[name=statement").val();
   	$('.userInput').append("<p>" + add + "</p>");
   });
+  $('#list').click(function() {
+  	$('.userInput').append("<ul><li></li><li></li><li></li></ul>");
+  });
+
 });
